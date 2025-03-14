f8db3b3257ff372b26e3d266c8e2d324e0a17e82
HomHom1 <homahomen2@gmail.com>
2025-03-14 16:48:28 -0500

    arduino BLE implementation

--- BluetoothTest/client/client.ino
+++ BluetoothTest/client/client.ino
@@ -1,24 +1,104 @@
-#include "BluetoothSerial.h"
+#include <ArduinoBLE.h>
 
-BluetoothSerial SerialBT;
-String receivedMessage = "";
+// UUIDs - must match peripheral
+const char* serviceUUID = "19B10000-E8F2-537E-4F6C-D104768A1214";
+const char* sendCharUUID = "19B10001-E8F2-537E-4F6C-D104768A1214";
+const char* receiveCharUUID = "19B10002-E8F2-537E-4F6C-D104768A1214";
+
+String messageToSend = "Hello from client";
+bool sentMessage = false;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32_Client"); 
-  Serial.println("The device started in client mode, waiting for connections...");
+  delay(1000);
+  
+  Serial.println("Starting BLE Client...");
+  
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+    while (1);
+  }
+  
+  Serial.println("BLE Central initialized, scanning for peripherals...");
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    char incomingChar = SerialBT.read();
-    if (incomingChar != '\n') {
-      receivedMessage += String(incomingChar);
+  BLE.scanForUuid(serviceUUID);
+  
+  BLEDevice peripheral = BLE.available();
+  
+  if (peripheral) {
+    Serial.print("Found peripheral: ");
+    Serial.println(peripheral.localName());
+    
+    BLE.stopScan();
+    
+    if (connectToPeripheral(peripheral)) {
+      Serial.println("Disconnected from server");
     } else {
-      Serial.println("Message from server: " + receivedMessage);
-      receivedMessage = "";
-      SerialBT.println("Hello from ESP32 clietn");
+      Serial.println("Connection failed");
     }
+    
+    // Restart scanning
+    Serial.println("Scanning for peripherals...");
+    sentMessage = false;
   }
-  delay(20);
+}
+
+bool connectToPeripheral(BLEDevice peripheral) {
+  Serial.println("Connecting...");
+  
+  if (!peripheral.connect()) {
+    return false;
+  }
+  
+  Serial.println("Connected");
+  
+  // Discover peripheral attributes
+  if (!peripheral.discoverAttributes()) {
+    Serial.println("Attribute discovery failed");
+    peripheral.disconnect();
+    return false;
+  }
+  
+  BLEService service = peripheral.service(serviceUUID);
+  if (!service) {
+    Serial.println("Service not found");
+    peripheral.disconnect();
+    return false;
+  }
+  
+  BLECharacteristic sendChar = service.characteristic(sendCharUUID);
+  BLECharacteristic receiveChar = service.characteristic(receiveCharUUID);
+  
+  if (!sendChar || !receiveChar) {
+    Serial.println("Characteristics not found");
+    peripheral.disconnect();
+    return false;
+  }
+  
+  if (!receiveChar.subscribe()) {
+    Serial.println("Subscription failed");
+    peripheral.disconnect();
+    return false;
+  }
+  
+  Serial.println("Subscribed to notifications");
+  
+  while (peripheral.connected()) {
+    if (!sentMessage) {
+      sendChar.writeValue(messageToSend);
+      Serial.println("Message sent: " + messageToSend);
+      sentMessage = true;
+    }
+    
+    if (receiveChar.valueUpdated()) {
+      String value = receiveChar.readValue();
+      Serial.println("Received: " + value);
+    }
+    
+    delay(100);
+  }
+   
+  return true;
 }
--- BluetoothTest/server/server.ino
+++ BluetoothTest/server/server.ino
@@ -1,26 +1,51 @@
-#include "BluetoothSerial.h"
+#include <ArduinoBLE.h>
 
-BluetoothSerial SerialBT;
-String receivedMessage = "";
+BLEService dataService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEStringCharacteristic sendCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 20);
+BLEStringCharacteristic receiveCharacteristic("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify, 20);
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32_Server"); 
-  Serial.println("The device started in server mode, make sure remote BT device is on!");
+  
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("ESP32_Server");
+  
+  BLE.setAdvertisedService(dataService);
+  dataService.addCharacteristic(sendCharacteristic);
+  dataService.addCharacteristic(receiveCharacteristic);
+  BLE.addService(dataService);
+  
+  sendCharacteristic.setValue("");
+  receiveCharacteristic.setValue("");
+
+  BLE.advertise();
+  Serial.println("BLE device active, waiting for connections...");
 }
 
 void loop() {
-  SerialBT.println("Hello from ESP32 server");
-  delay(1000);
+  BLEDevice central = BLE.central();
   
-  if (SerialBT.available()) {
-    char incomingChar = SerialBT.read();
-    if (incomingChar != '\n') {
-      receivedMessage += String(incomingChar);
-    } else {
-      Serial.println("Message from client: " + receivedMessage);
-      receivedMessage = "";
+  if (central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+    
+    while (central.connected()) {
+      if (sendCharacteristic.written()) {
+        String value = sendCharacteristic.value();
+        Serial.print("Received: ");
+        Serial.println(value);
+        
+        receiveCharacteristic.setValue("Response to: " + value);
+      }
+      
+      delay(10);
     }
+    
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
   }
-  delay(20);
 }
